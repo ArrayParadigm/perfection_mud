4c705ab16ff25af4ff9b2d724510fc190829899d
ArrayParadigm <131939514+ArrayParadigm@users.noreply.github.com>
2023-04-26 23:29:54 -0500

    Add files via upload

---
+++ MudRoom.java
@@ -0,0 +1,25 @@
+package perfection;
+
+public class MudRoom {
+    private int number;
+    private String name;
+    private String description;
+
+    public MudRoom(int number, String name, String description) {
+        this.number = number;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
